0dd0b003b1ff0f6263b119932fb406b11260d93d
Ben Fuqua <bfuqua@mail.greenriver.edu>
2021-05-13 14:27:52 -0400

    finalized index page with validation and data storage

--- index.php
+++ index.php
@@ -16,11 +16,62 @@ require_once ('model/data-layer.php');
 $f3 = Base::instance();
 
 //Define default route
-$f3->route('GET /', function(){
+$f3->route('GET|POST /', function($f3){
+
+    $userFlavors = array();
+    $userName = "";
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST')
+    {
+        $userFlavors = $_POST['flavors'];
+        $userName = $_POST['name'];
+
+        //If name is valid
+        if (validName($userName))
+        {
+            $_SESSION[$userName] = $userName;
+        }
+        else
+        {
+            $f3->set('errors["name"]', 'Please enter your name');
+        }
+
+        //If flavors are selected
+        if (validFlavors($userFlavors))
+        {
+            //Get user input
+            $_SESSION[$userFlavors] = implode(",", $userFlavors);
+        }
+        else
+        {
+            $f3->set('errors["flavors"]', 'Please select a flavor');
+        }
+    }
+
+    //If there are no errors redirect to summary route
+    if (empty($f3->get('errors')))
+    {
+        header('location: summary');
+    }
+
+    //Get the flavors from the Model and send them to the View
+    $f3->set('flavors', getFlavors());
+
+    //Store the user input in the hive
+    $f3->set('userName', $userName);
+    $f3->set('userFlavors', $userFlavors);
+
 
     //Display the home page
     $view = new Template();
     echo $view->render('views/home.html');
+});
+
+$f3->route('GET /summary', function(){
+
+    //Display the summary page
+    $view = new Template();
+    echo $view->render('views/summary.html');
 });
 
 //Run Fat-Free
