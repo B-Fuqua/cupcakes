6cf53d6e4cc06e4e0d97e3a18359c538e755642f
David Pavlenko <Dpavlenko@mail.greenriver.edu>
2021-05-13 13:30:44 -0400

    Added model directory with data-layer file which contains a function with an array of cupcakes

---
+++ model/data-layer.php
@@ -0,0 +1,7 @@
+<?php
+
+//associative array of cupcake function
+function getFlavors(){
+    return array("grasshopper" => "The Grasshopper", "maple" => "Whiskey Maple Bacon", "carrot" => "Carrot Walnut", "caramel" => "Salted Caramel Cupcake",
+        "velvet" => "Red Velvet", "lemon" => "Lemon Drops", "tiramisu" => "Tiramisu");
+}
